f38be62819bef27db3fad9b0a472b479c605f025
Andrea Albert <94383610+Andrearea21@users.noreply.github.com>
2023-02-23 13:08:27 +0100

    added bootstrap and axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-
+import "bootstrap";
+import Search from "./Search";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello from App</p>
       </header>
+      <div>
+        <Search />
+      </div>
     </div>
   );
 }
